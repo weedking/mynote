1ade12ff76f9ed4145d58e5f67ae30346eabad95
weedking <260815998@qq.com>
2019-04-18 18:00:52 +0800

    初始创建。

---
+++ src/main/java/com/note/mynote/MynoteApplication.java
@@ -0,0 +1,13 @@
+package com.note.mynote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MynoteApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/note/mynote/controller/MyController.java
@@ -0,0 +1,9 @@
+package com.note.mynote.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class MyController {
+}
