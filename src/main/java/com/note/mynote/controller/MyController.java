80f8637a148d9b222047c814e4065ebed7e0028c
weedking <260815998@qq.com>
2019-04-19 10:52:52 +0800

    react可用，增删查已有。

--- src/main/java/com/note/mynote/controller/MyController.java
+++ src/main/java/com/note/mynote/controller/MyController.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 
 @RestController
 @EnableAutoConfiguration
@@ -21,4 +23,33 @@ public class MyController {
         Note note = this.noteService.getNoteByNo(no1);
         return note;
     }
+
+    @RequestMapping("/addNote")//增加用户
+    @ResponseBody
+    @CrossOrigin
+    public Note addNote(@RequestParam(value = "no", required = false) int no,
+                        @RequestParam(value = "title", required = false) String title,
+                        @RequestParam(value = "author", required = false) String author,
+                        @RequestParam(value = "content", required = false) String content,
+                        @RequestParam(value = "creatTime", required = false) Date creatTime){
+        Note note = new Note();
+        note.setNo(no);
+        note.setTitle(title);
+        note.setAuthor(author);
+        note.setContent(content);
+        note.setCreateTime(creatTime);
+        this.noteService.addNote(note);
+        return note;
+    }
+
+    @RequestMapping("/deleteNote")//删除用户
+    @ResponseBody
+    @CrossOrigin
+    public int deleteNote(HttpServletRequest request,
+                          @RequestParam(value = "no",required = false) int no){
+        int no1 = Integer.parseInt(request.getParameter("no"));
+        this.noteService.deleteNote(no1);
+        return no1;//返回删除的行数
+    }
+
 }
