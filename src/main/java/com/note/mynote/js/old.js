a1f40543e041434b5ae0edfbc0a095886a5d1e9f
weedking <260815998@qq.com>
2019-04-26 17:43:47 +0800

    通过生命周期执行查询，并成功获取JSON数据。

--- src/main/java/com/note/mynote/js/old.js
+++ src/main/java/com/note/mynote/js/old.js
@@ -5,14 +5,39 @@ class Toggle extends React.Component {
         super(props);
         this.state = {
             // isToggleOn: true,
-            inputValue : '1',
+            inputValue : '好人',
             amount: 555,
-            content: ''
+            content: '',
+            title1:'',
+            date: new Date(),
+            id: 1
+
         };
 
         // This binding is necessary to make `this` work in the callback
         // this.handleClick = this.handleClick.bind(this);
         // this.changeInputValue = this.changeInputValue(this);
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+        // this.changeInputValue();
+        this.getNoteList();
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+
     }
 
     handleClick() {
@@ -30,8 +55,10 @@ class Toggle extends React.Component {
                 }
             }
         }
-        var preurl = "http://localhost:8080/addNote?no=10&content=";
+        var preurl = "http://localhost:8080/addNote?no=11&content=";
         var url = preurl + this.state.content;
+        // var url = preurl + this.state.id;
+
         // var url ="http://localhost:8080/showNote?no=1";
         var getInformation ={
             method:"POST",
@@ -48,10 +75,6 @@ class Toggle extends React.Component {
                 debugger;
                 console.log(responseJson)
                 this.setState({
-                    // inputValue:json[0]
-                    // object:json.object.list
-                    // inputValue:9994
-                    // inputValue:json.object.list
                     inputValue:responseJson.content
 
                 })
@@ -78,7 +101,7 @@ class Toggle extends React.Component {
         }
         var preurl = "http://localhost:8080/showNote?no=";
         var url = preurl + this.state.inputvalue;
-        // var url ="http://localhost:8080/showNote?no=1";
+        // var url ="http://localhost:8080/showNote?no=2";
         var getInformation ={
             method:"POST",
             headers:{
@@ -99,6 +122,39 @@ class Toggle extends React.Component {
                     // inputValue:9994
                     // inputValue:json.object.list
                     inputValue:responseJson.content
+
+                })
+            })
+    }
+
+//从后台获得title列表
+    getNoteList(){
+        /* 查询数据的格式 */
+        let filter={
+            object:{
+                object:{
+
+                }
+            }
+        }
+
+        var url ="http://127.0.0.1:8080/getNoteList";
+        var getInformation ={
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            /* json格式转换 */
+            body:JSON.stringify(filter)
+        }
+        fetch(url,getInformation)
+            .then(response => response.json())
+            .then(responseJson=>{
+                // 返回的数据 根据自己返回的json格式取值
+                debugger;
+                console.log(responseJson)
+                this.setState({
+                    title1:eval(responseJson)[0].title
 
                 })
             })
@@ -125,6 +181,15 @@ class Toggle extends React.Component {
             <div className="container">
                 <div className="leftbox">
                     <button className='newNote' onClick={this.NewNote.bind(this)} >新建</button>
+                    <h2>这是 {this.state.title1}.</h2>
+                    {/*{this.changeInputValue.bind(this)}*/}
+                    {/*<ul>*/}
+                        {/*{*/}
+                            {/*title.map(function (title) {*/}
+                                {/*return <li>{title}</li>*/}
+                            {/*})*/}
+                        {/*}*/}
+                    {/*</ul>*/}
                 </div>
 
                 <div className="rightbox">
@@ -143,6 +208,8 @@ class Toggle extends React.Component {
                         <h2>新笔记:</h2>
                         <textarea cols="30" rows="8"  ref='textarea' value={this.state.content} onChange = {this.handelNewNoteChange.bind(this)}/>
                         <button className='saveNewNote' onClick={this.saveNewNote.bind(this)} >保存</button>
+                        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+                        {/*{this.state.inputvalue}*/}
                     </p>
                     {/*笔记内容：*/}
                     {/*<p>{this.state.content}</p>*/}
