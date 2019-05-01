d317711b29babd4233b1ad64d4978ea67a25b170
weedking <260815998@qq.com>
2019-05-01 16:28:56 +0800

    可以读取数据库所有title字段。 清理了不需要的代码。

--- src/main/java/com/note/mynote/js/note.js
+++ src/main/java/com/note/mynote/js/note.js
@@ -1,58 +1,37 @@
-// import React, { Component } from 'react';
-
 class Toggle extends React.Component {
-
     constructor(props) {
         super(props);
         this.state = {
-            // isToggleOn: true,
-            inputValue : '好人',
-            amount: 555,
-            content: '',
-            title1:[],
-            title2:[],
-            date: new Date(),
-            id: 1,
-            listid:[],
-            // mynote: [{id:1,title: '张三'}, {id:2, title: '李四'}, {id: 3, title: "王五"}]
-            mynote: [{},{},{},{},{},{},{}],
-            // mynote:[...data]
-            clickid:''
+            inputValue : '',//查询结果
+            content: '',//笔记内容
+            date: new Date(),//当前时间
+            mynote: [],//存储笔记的相关字段内容
         };
 
         // This binding is necessary to make `this` work in the callback
-        // this.handleClick = this.handleClick.bind(this);
-        // this.changeInputValue = this.changeInputValue(this);
         this.getNoteList=this.getNoteList.bind(this);
         this.clickNoteTitle=this.clickNoteTitle.bind(this);
     }
 
-    componentDidMount() {
+    componentDidMount() {//组件挂载时执行的代码
         this.timerID = setInterval(
             () => this.tick(),
-            1000
+            1000 //每秒更新一次
         );
-
     }
 
-    tick() {
+    tick() {//定时器
         this.setState({
-            date: new Date()
+            date: new Date()//创建当前时间
         });
-        // this.changeInputValue();
         this.getNoteList();
     }
 
     componentWillUnmount() {
-        clearInterval(this.timerID);
+        clearInterval(this.timerID);//清理计时器
 
     }
 
-    handleClick() {
-        this.setState(prevState => ({
-            isToggleOn: !prevState.isToggleOn
-        }));
-    }
 
 //保存新笔记
     saveNewNote(){
@@ -63,11 +42,13 @@ class Toggle extends React.Component {
                 }
             }
         }
-        var preurl = "http://localhost:8080/addNote?no=11&content=";
-        var url = preurl + this.state.content;
-        // var url = preurl + this.state.id;
+        // Math.floor(Math.random()*(max-min+1)+min);//产生随机数
+        var createID=Math.floor(Math.random()*(1000-1+1)+1);
 
-        // var url ="http://localhost:8080/showNote?no=1";
+        var preurl1 = "http://localhost:8080/addNote?no=";
+        var preurl2= "&content=";
+        var url = preurl1+createID+preurl2 + this.state.content;//后台增加笔记的url地址
+
         var getInformation ={
             method:"POST",
             headers:{
@@ -91,7 +72,7 @@ class Toggle extends React.Component {
 
     //更新新笔记内容
     handelNewNoteChange(){
-        let value=this.refs.textarea.value
+        let value=this.refs.textarea.value;
         this.setState({
             content: value
         })
@@ -106,7 +87,7 @@ class Toggle extends React.Component {
 
                 }
             }
-        }
+        };
         var preurl = "http://localhost:8080/showNote?no=";
         var url = preurl + this.state.inputvalue;
         // var url ="http://localhost:8080/showNote?no=2";
@@ -117,7 +98,7 @@ class Toggle extends React.Component {
             },
             /* json格式转换 */
             body:JSON.stringify(filter)
-        }
+        };
         fetch(url,getInformation)
             .then(response => response.json())
             .then(responseJson=>{
@@ -125,19 +106,13 @@ class Toggle extends React.Component {
                 debugger;
                 console.log(responseJson)
                 this.setState({
-                    // inputValue:json[0]
-                    // object:json.object.list
-                    // inputValue:9994
-                    // inputValue:json.object.list
                     inputValue:responseJson.content
-
                 })
             })
     }
 
 //从后台获得title列表
     getNoteList(){
-        // let arr = []
         /* 查询数据的格式 */
         let filter={
             object:{
@@ -145,7 +120,7 @@ class Toggle extends React.Component {
 
                 }
             }
-        }
+        };
 
         var url ="http://127.0.0.1:8080/getNoteList";
         var getInformation ={
@@ -161,38 +136,11 @@ class Toggle extends React.Component {
             .then(responseJson=>{
                 // 返回的数据 根据自己返回的json格式取值.
                 debugger;
-                console.log(responseJson)
-                // var items= this.state.title2 //存放笔记标题
-                // var items2=this.state.listid //存放笔记id
-
-
-                var midtitle = this.state.mynote
-                // var midtitle=[]
-                for(let i=0;i<eval(responseJson).length;i++){
-                    // items[i]=eval(responseJson)[i].title
-                    // items2[i]=eval(responseJson)[i].no
-                    midtitle[i].title=eval(responseJson)[i].title
-                    midtitle[i].id=eval(responseJson)[i].no
-                    midtitle[i].content=eval(responseJson)[i].content
-
-                }
-                // this.state.mynote.concat(midtitle);
-                // items[3]=eval(responseJson)[1].title
-                // items=eval(responseJson).title
-
-
-
+                console.log(responseJson);
                 this.setState({
-                    title1:eval(responseJson)[0].title,
-                    // title2:eval(responseJson)[1].title
-                    // title2:items, //存放笔记标题
-                    // listid:items2,//存放笔记id
-
-                    mynote:midtitle, //存放笔记标题和ID
-
-
-
+                    mynote: responseJson
                 })
+
             })
     }
 
@@ -211,11 +159,10 @@ class Toggle extends React.Component {
         })
     }
 
-    clickNoteTitle(id,con){
+    clickNoteTitle(no,con){
         this.setState({
             // clickid: e.target.key,
             content: con,
-            // content: midtitle[1].title
 
         })
     }
@@ -225,27 +172,10 @@ class Toggle extends React.Component {
             <div className="container">
                 <div className="leftbox">
                     <button className='newNote' onClick={this.NewNote.bind(this)} >新建</button>
-                    {/*<p>这是 {this.state.title1}.</p>*/}
-                    {/*<p>这是 {this.state.title2}.</p>*/}
-
-                    {/*<ul>*/}
-                    {/*{*/}
-                    {/*// this.state.listid[1]*/}
-                    {/*this.state.title2.map(function(item){*/}
-                    {/*return <li className="nondot" key={this.state.listid}>{item}</li>*/}
-                    {/*})*/}
-                    {/*}*/}
-                    {/*</ul>,*/}
-                    {/*{midtitle.map(u => <div key={u.id}>{u.id}:{u.title}</div>)}*/}
-
                     {this.state.mynote.map(
-                        u => <div key={u.id} onClick={this.clickNoteTitle.bind(this,u.id,u.content)}>{u.id}:{u.title}</div>)
+                        //传递点中的ID
+                        u => <div key={u.no} onClick={this.clickNoteTitle.bind(this,u.no,u.content)}>{u.no}:{u.title}</div>)
                     }
-                    {/*{this.state.inputValue}*/}
-                    {/*{this.state.clickid}*/}
-                    {/*{this.state.mynote[2].content}*/}
-
-                    {/*{this.state.mynote.length}*/}
 
 
 
@@ -268,13 +198,7 @@ class Toggle extends React.Component {
                         <textarea cols="30" rows="8"  ref='textarea' value={this.state.content} onChange = {this.handelNewNoteChange.bind(this)}/>
                         <button className='saveNewNote' onClick={this.saveNewNote.bind(this)} >保存</button>
                         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-                        {/*{this.state.inputvalue}*/}
-
                     </p>
-                    {/*笔记内容：*/}
-                    {/*<p>{this.state.content}</p>*/}
-                    {/*查询关键字：*/}
-                    {/*<p>{this.state.inputvalue}</p>*/}
                 </div>
             </div>
         );
